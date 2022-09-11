57b7fa7415478c19be859c7c24bf4ee639c939f7
Ryochi <ryo1.t.zubu@gmail.com>
2022-09-12 08:27:19 +1000

    adding Decorators

--- src/item/item.controller.ts
+++ src/item/item.controller.ts
@@ -1,14 +1,28 @@
-import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item.dto';
 
 @Controller('item')
 export class ItemController {
-    @Get()
-    findAll(): string {
-        return 'Get all items';
-    }
+  @Get()
+  findAll(): string {
+    return 'Get All Items';
+  }
 
-    @Post
-    create():string {
-        return 'Create Item';
-    }
+  @Get(':id')
+  findOne(@Param() param): string {
+    return `Item ${param.id}`;
+  }
+
+  @Post()
+  create(@Body() createItemDto: CreateItemDto): string {
+    return `Create name: ${createItemDto.name} Description: ${createItemDto.description} Qty: ${createItemDto.qty}`;
+  }
 }
